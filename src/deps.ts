c04aa97af07b61fc1e4779bf21d92f0e6898182c
Eduardo García Maleta <egmaleta.secondary@proton.me>
2023-04-19 21:06:29 -0400

    import Manifest type

--- src/deps.ts
+++ src/deps.ts
@@ -1,2 +1,5 @@
-export { type PageProps } from "https://deno.land/x/fresh@1.1.5/server.ts";
+export type {
+  PageProps,
+  Manifest,
+} from "https://deno.land/x/fresh@1.1.5/server.ts";
 export { type JSX } from "https://esm.sh/preact@10.13.1/jsx-runtime";
