27af938fa9dafb6ea90ab257fcf29035b9913348
Eduardo García Maleta <egmaleta.secondary@proton.me>
2023-04-19 23:12:47 -0400

    s -> fileName

--- src/utils.ts
+++ src/utils.ts
@@ -1,19 +1,19 @@
-export const isLayout = (s: string) => {
-  return /^(_layout\.(tsx|jsx|ts|js))$/.test(s);
+export const isLayout = (fileName: string) => {
+  return /^(_layout\.(tsx|jsx|ts|js))$/.test(fileName);
 };
 
-export const is404 = (s: string) => {
-  return /^(_404\.(tsx|jsx|ts|js))$/.test(s);
+export const is404 = (fileName: string) => {
+  return /^(_404\.(tsx|jsx|ts|js))$/.test(fileName);
 };
 
-export const is500 = (s: string) => {
-  return /^(_500\.(tsx|jsx|ts|js))$/.test(s);
+export const is500 = (fileName: string) => {
+  return /^(_500\.(tsx|jsx|ts|js))$/.test(fileName);
 };
 
-export const isMiddleware = (s: string) => {
-  return /^(_middleware\.(tsx|jsx|ts|js))$/.test(s);
+export const isMiddleware = (fileName: string) => {
+  return /^(_middleware\.(tsx|jsx|ts|js))$/.test(fileName);
 };
 
-export const isApp = (s: string) => {
-  return /^(_app\.(tsx|jsx|ts|js))$/.test(s);
+export const isApp = (fileName: string) => {
+  return /^(_app\.(tsx|jsx|ts|js))$/.test(fileName);
 };
