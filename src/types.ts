d9db780035b2b536b9c1861106946d9555405ac6
Eduardo García Maleta <egmaleta.secondary@proton.me>
2023-04-19 21:10:29 -0400

    add Module and RouteInfo types

--- src/types.ts
+++ src/types.ts
@@ -4,7 +4,16 @@ import { JSX, PageProps } from "./deps.ts";
 export type Page<Data = any> = (props?: PageProps<Data>) => JSX.Element;
 
 // deno-lint-ignore no-explicit-any
-export type LayoutPage<Data = any> = (
+export type Layout<Data = any> = (
   child: Page<Data>,
   props?: PageProps<Data>
 ) => JSX.Element;
+
+export interface Module {
+  default?: Page | Layout;
+}
+
+export interface RouteInfo {
+  path: string;
+  module: Module;
+}
