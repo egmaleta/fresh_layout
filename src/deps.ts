6e0c2fe0f0465b559fc1b6a6e2d7ac25c1346e4a
Andy <the.andy.franklin@gmail.com>
2023-06-29 09:25:19 -0700

    updated to fresh@1.2.0

--- src/deps.ts
+++ src/deps.ts
@@ -1,11 +1,11 @@
 export type {
   Manifest,
   PageProps,
-} from "https://deno.land/x/fresh@1.1.6/server.ts";
+} from "https://deno.land/x/fresh@1.2.0/server.ts";
 export type {
   AppModule,
   ErrorPageModule,
   RouteModule,
   UnknownPageModule,
-} from "https://deno.land/x/fresh@1.1.6/src/server/types.ts";
+} from "https://deno.land/x/fresh@1.2.0/src/server/types.ts";
 export type { JSX } from "https://esm.sh/preact@10.13.1/";
